2da92be76e19ceb81059eddf4d8d588aa0c75ade
fiqz99 <66556674+fiqz99@users.noreply.github.com>
2020-12-21 03:15:08 +0100

    Update funkcije.js
    Greska prilikom prethodnog komita, prethodni put sam slucajno ostavio i delove iz starog koda. Sve promene kao sto sam naveo u prethodnom komitu:
    Dodat deo koda za pitanja koja zahtevaju unos sa tastature, kao i tajmer i obelezavanje tacnih i pogresnih odgovora nakon sto korisnik odgovori na pitanje, i ispis poena na kraju sa imenom koje je korisnik uneo.

--- hw-3/funkcije.js
+++ hw-3/funkcije.js
@@ -1,15 +1,4 @@
-var i=0;
-var c=0;
-var correctCount=0;
-btn1.style.visibility = 'hidden';
-btn2.style.visibility = 'hidden';
-btn3.style.visibility = 'hidden';
-optt1.style.visibility = 'hidden';
-optt2.style.visibility = 'hidden';
-optt3.style.visibility = 'hidden';
-opt1.style.visibility = 'hidden';
-opt2.style.visibility = 'hidden';
-opt3.style.visibility = 'hidden';var i=0; //brojac koraka
+var i=0; //brojac koraka
 var correctCount=0; //brojac tacnih odgovora
 var counter=20; //brojac vremena
 var stopCounter=1; //promenljiva koja sluzi za zaustavljanje vremena
@@ -189,54 +178,4 @@ function startTimer(){
 }
 	}
 setInterval(timeIt, 1000); //postavljanje intervala na 1 sekundu
-}
-question.style.visibility = 'hidden';
-
-function start(){
-		btn0.style.visibility = 'hidden';
-		btn1.style.visibility = 'visible';
-		btn2.style.visibility = 'visible';
-		btn3.style.visibility = 'visible';
-		optt1.style.visibility = 'visible';
-		optt2.style.visibility = 'visible';
-		optt3.style.visibility = 'visible';
-		opt1.style.visibility = 'visible';
-		opt2.style.visibility = 'visible';
-		opt3.style.visibility = 'visible';
-		question.style.visibility = 'visible';
-}
-
-generate(0);
-function generate(index){
-	document.getElementById("question").innerHTML=jsonData[index].q ;
-	document.getElementById("optt1").innerHTML=jsonData[index].opt1 ;
-	document.getElementById("optt2").innerHTML=jsonData[index].opt2 ;
-	document.getElementById("optt3").innerHTML=jsonData[index].opt3 ;
-}
-
-function checkAnswer(){
-	if (document.getElementById("opt1").checked && jsonData[i].opt1==jsonData[i].ans){
-		correctCount++;
-	}
-	if (document.getElementById("opt2").checked && jsonData[i].opt2==jsonData[i].ans){
-		correctCount++;
-	}
-	if (document.getElementById("opt3").checked && jsonData[i].opt3==jsonData[i].ans){
-		correctCount++;
-	}
-	i++;
-	
-	if (jsonData.length-1<i){
-		document.write("Your score is: " + correctCount);
-	}
-	generate(i);
-}
-function nextQuestion(){
-	i++;
-	if (jsonData.length-1<i){
-		document.write("Your score is: " + correctCount);
-	}
-}
-function quit(){
-	document.write("Your score is: " + correctCount);
 }
