dfb26dabc83330405f9ba9c32c36edee7232c4c6
fiqz99 <66556674+fiqz99@users.noreply.github.com>
2021-01-29 13:52:25 +0100

    Add files via upload
    osnovna funkcija promene strane u odnosu na to da li korisnik ima nalog ili zeli da se registruje

---
+++ hw-4/script.js
@@ -0,0 +1,10 @@
+register.style.display="none";
+
+function change1(){
+    register.style.display="inline";
+    logIn.style.display="none";
+}
+function change2(){
+    register.style.display="none";
+    logIn.style.display="inline";
+}
